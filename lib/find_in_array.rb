e3c422a2746a247ad19d84d04daea27f4822981a
Drew Beckmen <drew.beckmen@yale.edu>
2020-05-03 22:42:21 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,6 @@
 def find_element_index(array, value_to_find)
-  counter = 0 
-  while counter < array.length do 
-    if array[counter] == value_to_find
-      return counter 
-    else 
-      counter += 1
+  array.length.times do |index|
+    if array[index] == value_to_find
+      return index 
   end 
 end
